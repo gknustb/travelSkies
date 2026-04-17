83749c0c2a24ae7e9615e0b9ec984c9fdc14900d
Guilherme Knust <gknust.b@gmail.com>
2026-04-17 14:50:24 -0300

    chore: cleaned up a little for merge

--- src/main/java/com/gknust/App.java
+++ src/main/java/com/gknust/App.java
@@ -1,5 +1,4 @@
 package com.gknust;
-import com.gknust.*;
 import com.gknust.db.*;
 import com.gknust.db.dao.*;
 import com.gknust.api.*;
@@ -7,24 +6,14 @@ import com.gknust.model.*;
 import com.gknust.util.DateMath;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.time.*;
 
 public class App {
-
-    //private static final String dbUrl = "jdbc:sqlite:database.db";
-
     public static void main(String[] args) {
-//        var fetcher = new WeatherFetcher(-15.799612655608906, -47.86418960896907, "2026-04-08");
-//        try{
-//            WeatherResponse weather = fetcher.getWeather();
-//            System.out.println(weather.daily().temperature_2m_mean()[0]);
-//        }catch(Exception e){
-//            e.printStackTrace();
-//        }
 
-        //todo: create controller layer for each data object for proper encapsulation
+        //todo: create DTOs for the data models
         //todo: create service layers to receive controller layer inputs and talk to DAOs
+        //todo: create controller layer for each data object for proper encapsulation
         //todo: unit tests
         //todo: UI layer
 
